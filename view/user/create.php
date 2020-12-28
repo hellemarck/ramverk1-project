cf0225eecd7ddbbafab5ed8e998f8e7aa671fee5
Mette Hellemarck <mettehellemarck@hotmail.com>
2020-12-28 15:49:43 +0100

    ...

---
+++ view/user/create.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Anax\View;
+
+?>
+<article class="article">
+<div class="questions user">
+
+<h1>Skapa användare</h1>
+
+<?= $form ?>
+
+</div>
+</article>
---
+++ view/user/login.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Anax\View;
+
+?>
+<article class="article">
+<div class="questions user">
+
+<h1>Logga in</h1>
+
+<?= $form ?>
+
+</div>
+</article>
